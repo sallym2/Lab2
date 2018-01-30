da65297a5ceb94a4492c1f53e94c0dcf8394580c
Sally Ming <sallym2@illinois.edu>
2018-01-30 16:46:11 -0600

    Finished maze code.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,26 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                if (!maze.canMove()) {
+                    maze.turnRight();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnRight();
+                        if (maze.canMove()) {
+                            maze.move();
+                        } else {
+                            maze.turnRight();
+                            maze.move();
+                        }
+                    }
+                } else {
+                    maze.move();
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
